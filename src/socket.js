29efbb9de7981da2e8fdbd27f42f2fb3af1baa1b
Gameinwza <apunbunchu@gmail.com>
2026-03-19 17:45:49 +0700

    fix api url

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 // เปลี่ยน URL ตรงนี้จุดเดียวถ้า deploy
-export const socket = io('http://localhost:3001', {
+export const socket = io('http://129.150.48.168:3001', {
   autoConnect: true,
   reconnection: true,
   reconnectionDelay: 1000,
